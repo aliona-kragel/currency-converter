1925f6a14e4e7b1a5ac08637a6811c9cc273a47c
Aliona Kragel <aliona.kragel@mail.ru>
2023-10-25 22:33:18 +0300

    Fixed bug

--- client/src/services/index.ts
+++ client/src/services/index.ts
@@ -1,17 +1,18 @@
 import axios from 'axios';
-import { API_URL } from 'helpers/constants';
+import { getApiUrl } from 'helpers/constants';
+
 
 const currencyService = {
   async getCurrencies() {
-    const { data } = await axios.get(`${API_URL}/Currencies`);
+    const { data } = await axios.get(`${getApiUrl()}/Currencies`);
     return data
   },
   async getShortedCurrencies() {
-    const { data } = await axios.get(`${API_URL}/ShortedCurrencies`);
+    const { data } = await axios.get(`${getApiUrl()}/ShortedCurrencies`);
     return data
   },
   async updateCurrencies(currData: { abbr: string, amount: number }) {
-    const { data } = await axios.post(`${API_URL}/UpdateCurrencies`, currData);
+    const { data } = await axios.post(`${getApiUrl()}/UpdateCurrencies`, currData);
     return data;
   }
 }
