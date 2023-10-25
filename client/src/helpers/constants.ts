984f35ad140d28e09df20de1cab80526fc19be1e
Aliona Kragel <aliona.kragel@mail.ru>
2023-10-25 23:25:55 +0300

    Changed constants

--- client/src/helpers/constants.ts
+++ client/src/helpers/constants.ts
@@ -3,9 +3,9 @@ export const DEFAULT_AMOUNT = 1;
 export const DEFAULT_ABBR = 'USD';
 
 export const getApiUrl = () => {
-  return process.env.API_URL || 'http://localhost:3001'
+  return process.env.REACT_APP_API_URL || 'https://localhost:3001'
 }
 
 export const getClientDomain = () => {
-  return process.env.CLIENT_DOMAIN || "http://localhost:3000/"
+  return process.env.REACT_APP_CLIENT_DOMAIN || "https://localhost:3000/"
 }
